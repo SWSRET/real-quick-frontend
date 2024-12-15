fef4681c91c100bd37206282d08a2a6a1195fbae
Divineleo2001 <divya@atparui.com>
2024-12-15 14:54:06 +0530

    fix:linting was throwing an issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  modules: ['@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
-  colorMode: {
-    preference: 'dark', // default value of $colorMode.preference
-    fallback: 'light', // fallback value if not system preference found
-    storage: 'cookie', // or 'sessionStorage' or 'cookie'
-    storageKey: 'nuxt-color-mode',
-  },
 })
